8d4e5fb112989e5cd27626b1f8d2bb32d9f4ebed
ppopow <pawel.popow@gmail.com>
2020-10-19 14:23:17 +0200

    nowe zadanie35

---
+++ js/main.js
@@ -0,0 +1,15 @@
+function sprawdzLiczbe(n) {
+    let result = null;
+
+    if ((n>=100) && (n<=200)) {
+
+        console.log("Liczba znajduje się w przedziale");
+    } else {
+        console.log("Liczba nie znajduje się w przedziale");
+        
+    }
+}
+
+
+sprawdzLiczbe(108);
+sprawdzLiczbe(1000);
